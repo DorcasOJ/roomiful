537eeaefbfecab55873ea5dd654264ac08b3b611
Dorcas <ayanfedorcas2017@gmail.com>
2026-02-26 16:21:49 +0100

    modify homepage ui

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import type { Route } from "./+types/home";
 import Navbar from "../../components/Navbar";
+import { ArrowRight, ArrowUpRight, Clock, Layers } from "lucide-react";
+import Button from "components/ui/Button";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -10,9 +12,97 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return (
-      <div className={'home'}>
-          <Navbar/>
-        <h1 className={'text-3xl text-indigi-800 font-bold'}>Hello</h1>
-      </div>
+    <div className={"home"}>
+      <Navbar />
+
+      <section className="hero">
+        <div className="announce">
+          <div className="dot">
+            <div className="pulse"></div>
+          </div>
+
+          <p>Introducing Roomiful 2.0</p>
+        </div>
+
+        <h1>Build awesome spaces with a speed of thought with Roomiful</h1>
+
+        <p className="subtitle">
+          Roomiful is an AI-first design environment that helps you visualize,
+          render, and ship, architectural projects faster than ever.
+        </p>
+
+        <div className="actions">
+          <a href="#upload" className="cta">
+            Start Building <ArrowRight className="icon" />
+          </a>
+
+          <Button variant="outline" size="lg" className="demo">
+            Watch demo
+          </Button>
+        </div>
+
+        <div className="upload-shell" id="upload">
+          <div className="grid-overlay" />
+
+          <div className="upload-card">
+            <div className="upload-head">
+              <div className="upload-icon">
+                <Layers className="icon" />
+              </div>
+
+              <h3>Upload your floor plan</h3>
+              <p>Supports JPG, PNG, formats up to 10MB</p>
+            </div>
+
+            <p>Upload images</p>
+          </div>
+        </div>
+      </section>
+
+      <section className="projects">
+        <div className="section-inner">
+          <div className="section-head">
+            <div className="copy">
+              <h2>Projects</h2>
+              <p>
+                Your latest work and shared community projects, all in one
+                place.
+              </p>
+            </div>
+          </div>
+
+          <div className="projects-grid">
+            <div className="project-card group">
+              <div className="preview">
+                <img
+                  src="https://images.unsplash.com/photo-1721244653721-bc681b2dfd27?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8YXJjaGl0ZWN0dXJhbCUyMGRyYXdpbmd8ZW58MHx8MHx8fDA%3D"
+                  alt="Project"
+                />
+
+                <div className="badge">
+                  <span>Community</span>
+                </div>
+              </div>
+
+              <div className="card-body">
+                <div>
+                  <h3>Project RedCover</h3>
+
+                  <div className="meta">
+                    <Clock size={12} />
+                    <span>{new Date("01.01.2027").toLocaleDateString()}</span>
+                    <span>By Casdore</span>
+                  </div>
+                </div>
+
+                <div className="arrow">
+                  <ArrowUpRight size={18} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 }
